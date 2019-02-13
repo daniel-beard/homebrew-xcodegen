2371d3488c0d355e42ab79c87a5fe1f970465ac3
dbeard <dbeard@groupon.com>
2019-02-13 13:00:39 -0800

    Add bottle and update formula

--- Formula/xcodegen@2.1.0.rb
+++ Formula/xcodegen@2.1.0.rb
@@ -5,12 +5,11 @@ class XcodegenAT210 < Formula
   sha256 "b7895b8daacb7cef19e8dd3518ac060e9d9ba99479aff4a5b6ca44f7326099da"
   version "2.1.0"
 
-  # bottle do
-  #   root_url "https://artifacts-generic.groupondev.com/mobile/bottles-mobile"
-  #   cellar :any_skip_relocation
-  #   sha256 "f53875de6521b7b7fe94a13e9e5fa935165c70de92b78fc8455afdaabd63099a" => :mojave
-  #   sha256 "7b8a0f99b291777d57ebfdd0b65302263f93d7abcf29e2bdca2158fb5644d2dd" => :high_sierra
-  # end
+  bottle do
+    root_url "https://raw.githubusercontent.com/dbeard/homebrew-xcodegen/master/bottles"
+    cellar :any_skip_relocation
+    sha256 "29fbfde654ab43730cb878294e021b2ba1cd8a41e9c23bd1006541e7d4de3e1f" => :mojave
+  end
   
   depends_on :xcode => ["9.3", :build]
 
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-xcodegen
+
+## Bottle build commands 
+
+```shell
+brew install --build-bottle xcodegen@2.1.0
+brew bottle --root-url="https://raw.githubusercontent.com/dbeard/homebrew-xcodegen/master/bottles" xcodegen@2.1.0
+```
